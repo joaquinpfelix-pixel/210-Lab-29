be5a71ffdbceac201bd7c186c7c99d252b4d65f3
JoaquinF <joaquinpfelix@gmail.com>
2026-04-15 20:25:46 -0700

    Check 1

--- 210-Lab-29.cpp
+++ 210-Lab-29.cpp
@@ -1,1 +1,17 @@
 // COMSC-210 | Lab 29 | Joaquin Felix
+#include <iostream>
+#include <string>
+#include <array>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+// Constants
+const int VEHICLE_TYPES = 3;
+const int TIME_PERIODS = 25;
+const int MIN_FILES_LINES = 100;
+const string INPUT_FILE = "traffic_data.txt";
