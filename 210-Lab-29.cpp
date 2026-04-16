9a9af47974f10b56d304578d2c9e5cd2e5e1e6fa
JoaquinF <joaquinpfelix@gmail.com>
2026-04-15 20:34:33 -0700

    Check 2

--- 210-Lab-29.cpp
+++ 210-Lab-29.cpp
@@ -15,3 +15,44 @@ const int VEHICLE_TYPES = 3;
 const int TIME_PERIODS = 25;
 const int MIN_FILES_LINES = 100;
 const string INPUT_FILE = "traffic_data.txt";
+
+// Function prototypes
+void dispalyMap(
+    const map<string, array<list<string>, VEHICLE_TYPES>> & traffic_map);
+
+void run_simulation();
+
+
+int main () 
+{
+
+}
+
+void displayMap(
+    const map<string, array<list<string>, VEHICLE_TYPES>> & traffic_map)
+{
+    for (auto &entry : traffic_map)
+    {
+        cout << "Time Period: " << entry.first << endl;
+
+        cout << "Cars: ";
+        for (auto &v : entry.second[0])
+        {
+            cout << v << " ";
+        }
+
+        cout << "\n Buses: ";
+        for (auto &v : entry.second[1])
+        {
+            cout << v << " ";
+        }
+
+        cout << "\n Emergency Vehicles: ";
+        for (auto &v : entry.second[2])
+        {
+            cout << v << " ";
+        }
+
+        cout << "\n-----------------------------\n";
+    }
+}
